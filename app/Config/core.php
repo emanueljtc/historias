1827404716c3d6ce95de48d0a9a91a64ae187b21
Migue Cabrera <miguel4ngel@gmail.com>
2014-11-21 17:24:29 -0300

    modificado el valor de salt y cipher seed

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@ Configure::write('Routing.prefixes', array('admin'));
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYgh93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYgh93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi100');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76459529657453542496749683645');
+	Configure::write('Security.cipherSeed', '76459529657453542496749683645100');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
